f8392bfdc518215803aaeeef3c007ddcb513e036
Anna Sotnikova <ruceba77@gmail.com>
2022-01-16 22:20:49 +0600

    commit

--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -1,5 +1,6 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -7,25 +8,26 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
+
 public class CardDeliveryTest {
-    String date = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    String date = LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
 
     @Test
-    public void shouldReturnResultWithCorrectlyValues() {
-        open("http://localhost:9999");
-        $("[data-test-id='city'] input").setValue("Омск");
-        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").setValue(date);
-        $("[data-test-id='name'] input").setValue("Иван Петров");
-        $("[data-test-id='phone'] input").setValue("+78884938474");
-        $("[data-test-id='agreement']").click();
-        $$("button").find(exactText("Забронировать")).click();
-        $(".notification__content").shouldBe(visible, Duration.ofSeconds(15))
-                .shouldHave(exactText("Встреча успешно забронирована на " + date));
-
+            public void shouldSuccessfulFormSubmission () {
+            open("http://localhost:9999");
+            $("[data-test-id=city] input").setValue("Омск");
+            $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE);
+            String verificationDate = LocalDate.now().plusDays(4)
+                    .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+            $("[data-test-id=date] input").setValue(verificationDate);
+            $("[data-test-id=name] input").setValue("Иван Петров");
+            $("[data-test-id=phone] input").setValue("+79012345678");
+            $("[data-test-id=agreement]").click();
+            $(".button").shouldHave(Condition.text("Забронировать")).click();
+            $("[data-test-id=notification]")
+                    .shouldHave(Condition.text("Успешно! Встреча успешно забронирована на " + verificationDate),
+                            Duration.ofSeconds(15));
+        }
     }
-}
